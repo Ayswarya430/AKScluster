e0122dedd686be7e4eff4e1f807be692534625d3
Ayswarya430 <98088980+Ayswarya430@users.noreply.github.com>
2022-11-01 10:47:59 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -15,6 +15,5 @@ terraform {
 
 provider "azurerm" {
   features {}
-  aks_service_principal_app_id = "05124d5c-e54b-47d7-8a87-b4c0a4545106"
-aks_service_principal_client_secret = "Zgj8Q~u2_LErctPkOuJ03Ghcv31EIqUkwwOkZb2U"
+
 }
