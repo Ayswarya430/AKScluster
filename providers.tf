41f6ae6b08d35f68e817abc45e45b9e839aa333b
Ayswarya430 <98088980+Ayswarya430@users.noreply.github.com>
2022-11-01 11:15:00 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -15,11 +15,10 @@ terraform {
 
 provider "azurerm" {    
   features {}
-
-  subscription_id = 88b63171-abae-4af0-83c6-be7ef1e40edb
-  tenant_id       = d110c826-4e7b-4e0b-9090-2e63efaa6945
-  client_id       = 05124d5c-e54b-47d7-8a87-b4c0a4545106
-  client_secret   = Zgj8Q~u2_LErctPkOuJ03Ghcv31EIqUkwwOkZb2U
+  subscription_id = "88b63171-abae-4af0-83c6-be7ef1e40edb"
+  tenant_id       = "d110c826-4e7b-4e0b-9090-2e63efaa6945"
+  client_id       = "05124d5c-e54b-47d7-8a87-b4c0a4545106"
+  client_secret   = "Zgj8Q~u2_LErctPkOuJ03Ghcv31EIqUkwwOkZb2U"
 }
   
   
